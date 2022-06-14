6dfc3d9ac1e2b779567ca12a9036a8f6fa94f925
Navid66 <navidreza668@gmail.com>
2022-06-15 00:03:22 +0600

    Edit,Update,Delete Done For Category,Post Option
    Full Page Ready. Blog Post  Website

--- includes/category.php
+++ includes/category.php
@@ -1,7 +1,7 @@
 <div class="col-lg-12">
                   <div class="sidebar-item categories">
                     <div class="sidebar-heading">
-                      <h2>Categories</h2>
+                      <h2>Categorie's</h2>
                     </div>
                     <div class="content">
                       <ul>
